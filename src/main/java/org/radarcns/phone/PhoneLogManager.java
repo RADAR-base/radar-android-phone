67d99f53c33b493aecb1284054063d854cb2b3c0
Maxim Moinat <maxim@thehyve.nl>
2017-06-09 12:38:45 +0200

    Cleaned code and separate control for reading log history upon new install

--- src/main/java/org/radarcns/phone/PhoneLogManager.java
+++ src/main/java/org/radarcns/phone/PhoneLogManager.java
@@ -32,12 +32,14 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Base64;
 import android.util.SparseArray;
+import android.widget.Toast;
 
 import org.radarcns.android.data.DataCache;
 import org.radarcns.android.data.TableDataHandler;
 import org.radarcns.android.device.AbstractDeviceManager;
 import org.radarcns.android.device.BaseDeviceState;
 import org.radarcns.android.device.DeviceStatusListener;
+import org.radarcns.android.util.Boast;
 import org.radarcns.key.MeasurementKey;
 import org.radarcns.util.Serialization;
 import org.slf4j.Logger;
@@ -65,7 +67,9 @@ public class PhoneLogManager extends AbstractDeviceManager<PhoneLogService, Base
     private static final String LAST_CALL_KEY = "last.call.time";
     private static final String HASH_KEY = "hash.key";
     private static final String ACTIVITY_LAUNCH_WAKE = "ACTIVITY_LAUNCH_WAKE";
+    private static final int REQUEST_CODE_PENDING_INTENT = 1;
     private static final long CALL_SMS_LOG_INTERVAL_DEFAULT = 24 * 60 * 60; // seconds
+    private static final long CALL_SMS_LOG_HISTORY_DEFAULT = 24 * 60 * 60; // seconds
 
     static {
         CALL_TYPES.append(CallLog.Calls.INCOMING_TYPE, PhoneCallType.INCOMING);
@@ -115,18 +119,15 @@ public class PhoneLogManager extends AbstractDeviceManager<PhoneLogService, Base
 
     public void start(@NonNull Set<String> acceptableIds) {
         // Calls and sms, in and outgoing
-        setCallLogUpdateRate(CALL_SMS_LOG_INTERVAL_DEFAULT);
-        setSmsLogUpdateRate(CALL_SMS_LOG_INTERVAL_DEFAULT);
+        setCallAndSmsLogUpdateRate(CALL_SMS_LOG_INTERVAL_DEFAULT);
 
         updateStatus(DeviceStatusListener.Status.CONNECTED);
     }
 
-    public final synchronized void setCallLogUpdateRate(final long period) {
-        // Create an intent and alarm that will be wrapped in PendingIntent
-        Intent intent = new Intent(ACTIVITY_LAUNCH_WAKE);
-
-        // Create the pending intent and wrap our intent
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.context, 1, intent, 0);
+    public final synchronized void setCallAndSmsLogUpdateRate(final long period) {
+        // Create pending intent, which cancels currently active pending intent
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.context, REQUEST_CODE_PENDING_INTENT,
+                new Intent(ACTIVITY_LAUNCH_WAKE), PendingIntent.FLAG_CANCEL_CURRENT);
 
         // Get alarm manager and schedule it to run every period (seconds)
         AlarmManager alarmManager = (AlarmManager) this.context.getSystemService(ALARM_SERVICE);
@@ -136,24 +137,28 @@ public class PhoneLogManager extends AbstractDeviceManager<PhoneLogService, Base
         this.context.registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                processCall(System.currentTimeMillis() / 1000L - period);
+                processCallLog();
+                processSmsLog();
             }
         }, new IntentFilter(ACTIVITY_LAUNCH_WAKE));
 
-        logger.info("Call log: listener activated and set to a period of {}", period);
+        logger.info("Call and SMS log: listener activated and set to a period of {}", period);
     }
 
-    private synchronized void processCall(long threshold) {
-//      final long initialDateRead = Long.parseLong(storage.getOrSet(LAST_CALL_KEY, "0"));
+    private synchronized void processCallLog() {
         final long initialDateRead = Long.parseLong(preferences.getString(LAST_CALL_KEY, "0"));
 
+        // If this is the first call (initialDateRead is default),
+        // then the lastDateRead is a set time before current time.
         long lastDateRead = initialDateRead;
-        if (lastDateRead < threshold) {
-            lastDateRead = threshold;
+        if (initialDateRead == 0) {
+            lastDateRead = System.currentTimeMillis() - CALL_SMS_LOG_HISTORY_DEFAULT * 1000;
         }
 
         if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: handle case where permission is NOT granted
+//            SanityManager.getInstance(context).addPermissionAlert(this.getClass().getName(), Manifest.permission.READ_CALL_LOG, "", null);
+//            ActivityCompat.requestPermissions(this.context, new String[]{Manifest.permission.READ_CALL_LOG},0);
+            Boast.makeText(this.context, "Cannot read call log; permission not granted", Toast.LENGTH_LONG);
             return;
         }
         try (Cursor c = getService().getContentResolver().query(CallLog.Calls.CONTENT_URI, null, CallLog.Calls.DATE + " > " + lastDateRead, null, CallLog.Calls.DATE + " ASC")) {
@@ -177,7 +182,6 @@ public class PhoneLogManager extends AbstractDeviceManager<PhoneLogService, Base
         } finally {
             if (lastDateRead != initialDateRead) {
                 preferences.edit().putString(LAST_CALL_KEY, Long.toString(lastDateRead)).apply();
-//                storage.put(LAST_CALL_KEY, Long.toString(lastDateRead));
             }
         }
     }
@@ -191,40 +195,25 @@ public class PhoneLogManager extends AbstractDeviceManager<PhoneLogService, Base
         double timestamp = System.currentTimeMillis() / 1000d;
         send(callTable, new PhoneCall(eventTimestamp, timestamp, duration, ByteBuffer.wrap(targetKey), type));
 
-        logger.debug("Call log: {}, {}, {}, {}, {}, {}", target, targetKey, duration, type, eventTimestamp, timestamp);
+        logger.info("Call log: {}, {}, {}, {}, {}, {}", target, targetKey, duration, type, eventTimestamp, timestamp);
     }
 
-    public final synchronized void setSmsLogUpdateRate(final long period) {
-        // Create an intent and alarm that will be wrapped in PendingIntent
-        Intent intent = new Intent(ACTIVITY_LAUNCH_WAKE);
-
-        // Create the pending intent and wrap our intent
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.context, 1, intent, 0);
-
-        // Get alarm manager and schedule it to run every period (seconds)
-        AlarmManager alarmManager = (AlarmManager) this.context.getSystemService(ALARM_SERVICE);
-        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), period * 1000, pendingIntent);
-
-        // Activity to perform when alarm is triggered
-        this.context.registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                processSMS(System.currentTimeMillis() / 1000L - period);
-            }
-        }, new IntentFilter(ACTIVITY_LAUNCH_WAKE));
-
-        logger.info("SMS log: listener activated and set to a period of {}", period);
-    }
-
-    private synchronized void processSMS(long threshold) {
-//            final long initialDateRead = Long.parseLong(storage.getOrSet(LAST_SMS_KEY, "0"));
+    private synchronized void processSmsLog() {
         final long initialDateRead = Long.parseLong(preferences.getString(LAST_SMS_KEY, "0"));
 
+        // If this is the first call (initialDateRead is default),
+        // then the lastDateRead is a set time before current time.
         long lastDateRead = initialDateRead;
-        if (lastDateRead < threshold) {
-            lastDateRead = threshold;
+        if (initialDateRead == 0) {
+            lastDateRead = System.currentTimeMillis() - CALL_SMS_LOG_HISTORY_DEFAULT * 1000;
         }
 
+        if (ActivityCompat.checkSelfPermission(this.context, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+//            SanityManager.getInstance(context).addPermissionAlert(this.getClass().getName(), Manifest.permission.READ_CALL_LOG, "", null);
+//            ActivityCompat.requestPermissions(this.context, new String[]{Manifest.permission.READ_CALL_LOG},0);
+            Boast.makeText(this.context, "Cannot read sms log; permission not granted", Toast.LENGTH_LONG);
+            return;
+        }
         // Query all sms with a date later than the last date seen and sort by date
         try (Cursor c = getService().getContentResolver().query(Telephony.Sms.CONTENT_URI, null, Telephony.Sms.DATE + " > " + lastDateRead, null, Telephony.Sms.DATE + " ASC")) {
             if (c == null) {
@@ -233,8 +222,6 @@ public class PhoneLogManager extends AbstractDeviceManager<PhoneLogService, Base
 
             while (c.moveToNext()) {
                 long date = c.getLong(c.getColumnIndex(Telephony.Sms.DATE));
-
-//                logger.info("Read int: {}",  c.getString(c.getColumnIndex(Telephony.Sms.READ)));
 
                 sendPhoneSms(date / 1000d,
                         c.getString(c.getColumnIndex(Telephony.Sms.ADDRESS)),
@@ -248,7 +235,6 @@ public class PhoneLogManager extends AbstractDeviceManager<PhoneLogService, Base
         } finally {
             if (lastDateRead != initialDateRead) {
                 preferences.edit().putString(LAST_SMS_KEY, Long.toString(lastDateRead)).apply();
-//                    storage.put(LAST_SMS_KEY, Long.toString(lastDateRead));
             }
         }
     }
@@ -270,7 +256,7 @@ public class PhoneLogManager extends AbstractDeviceManager<PhoneLogService, Base
      * Extracts last 9 characters and hashes the result with a salt.
      * For phone numbers this means that the area code is removed
      * E.g.: +31232014111 becomes 232014111 and 0612345678 becomes 612345678 (before hashing)
-     * If target is a name instead of a number (e.g. when sms), then hash this raw name
+     * If target is a name instead of a number (e.g. when sms), then hash this name
      * @param target String
      * @return String
      */
